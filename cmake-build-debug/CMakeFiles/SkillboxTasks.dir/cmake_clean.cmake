05e86e35a7b1f73d2a1316a1bb35d15e4c0ba4ac
Andrei <andrei1996@cracken1996.com>
2021-07-08 18:42:05 +0100

    M11T5 and CMakeLists.txt files added

---
+++ cmake-build-debug/CMakeFiles/SkillboxTasks.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SkillboxTasks.dir/M11T1.cpp.o"
+  "CMakeFiles/SkillboxTasks.dir/M11T4.cpp.o"
+  "CMakeFiles/SkillboxTasks.dir/M11T5.cpp.o"
+  "SkillboxTasks"
+  "SkillboxTasks.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SkillboxTasks.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
