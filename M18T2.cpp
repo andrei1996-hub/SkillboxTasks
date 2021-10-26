a3b90f2c825169a23a80e7709d5202f5c8f9e5a3
andrei1996-hub <82802387+andrei1996-hub@users.noreply.github.com>
2021-10-26 07:11:14 +0100

    Update M18T2.cpp

--- M18T2.cpp
+++ M18T2.cpp
@@ -2,3 +2,28 @@
 // Created by andrei1996 on 25/10/2021.
 //
 
+#include <iostream>
+
+using namespace std;
+
+int hopComboCount(int stepsTotal = 3, int stepsMax = 3) {
+    int result = 0;
+    if (stepsTotal == 0) return 1;
+    else {
+        for (int i = 1; i <= min(stepsTotal, stepsMax); ++i) {
+            result += hopComboCount(stepsTotal-i, stepsMax);
+        }
+        return result;
+    }
+}
+
+int main() {
+    cout << "=====Jump-combo counter=====\n\n";
+
+    int stepsTotal;
+
+    cout << "Enter steps quantity: ";
+    cin >> stepsTotal;
+
+    cout << "Ways to do: " << hopComboCount(stepsTotal) << endl;
+}
