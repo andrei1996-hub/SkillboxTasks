889a3bfb65ce51b5cb449d33bc75e690c0e6a9bb
andrei1996-hub <82802387+andrei1996-hub@users.noreply.github.com>
2021-10-26 07:12:27 +0100

    Update M18T4.cpp

--- M18T4.cpp
+++ M18T4.cpp
@@ -2,3 +2,24 @@
 // Created by andrei1996 on 26/10/2021.
 //
 
+#include <iostream>
+
+using namespace std;
+
+void evenDigits(long long num, int& ans) {
+    if (num % 2 == 0) ++ans;
+    num /= 10;
+    if(num == 0) return;
+    evenDigits(num, ans);
+}
+
+int main() {
+    cout << "=====Even Digits Counter=====";
+
+    int ans = 0;
+    long long val = 9'223'372'036'854'775'806;
+
+    evenDigits(val, ans);
+
+    cout << "Even Digits: " << ans << endl;
+}
